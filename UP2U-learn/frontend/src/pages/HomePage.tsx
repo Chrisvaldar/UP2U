b4f8b1d8a95a0e695336e70ea21c73484b001fba
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-13 17:29:00 +1000

    simple handleCreate in HomePage

--- UP2U-learn/frontend/src/pages/HomePage.tsx
+++ UP2U-learn/frontend/src/pages/HomePage.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"; 
+
+
+const API_BASE = "http://localhost:8000";
+
 export default function HomePage() {
-    return <div>Home</div>
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const navigate = useNavigate();
+
+  async function handleCreate(){
+    const response = await axios.post(`${API_BASE}/create-session`, {hostname: name.trim()})
+    
+    const sessionCode = response.data.code
+    navigate(`/lobby/${sessionCode}`)
   }
+
+  return (
+    <div>
+      <h1>UP2U</h1>
+
+      <input
+        placeholder="your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button>Create Session</button>
+
+      <input
+        placeholder="code"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button>Join Session</button>
+    </div>
+  );
+}
