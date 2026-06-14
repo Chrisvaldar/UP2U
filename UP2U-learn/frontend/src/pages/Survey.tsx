1f87001eeaef4167fdca9c52cc879bd5179a08a9
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-14 21:00:27 +1000

    feat: useStates for Survey

--- UP2U-learn/frontend/src/pages/Survey.tsx
+++ UP2U-learn/frontend/src/pages/Survey.tsx
@@ -1,3 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
+
 export default function Survey() {
-    return <div>Survey</div>
-  }
+  const [hunger, setHunger] = useState(3);
+  const [vibe, setVibe] = useState("");
+  const [cuisinesRanked, setCuisinesRanked] = useState([]);
+  const [travelDistance, setTravelDistance] = useState("");
+  const [dietary, setDietary] = useState([]);
+  return <div>Survey</div>;
+}
