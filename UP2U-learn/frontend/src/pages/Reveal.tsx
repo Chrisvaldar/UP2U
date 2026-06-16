992fac0a3ed6d211ac05834f11ca03da60a8d69b
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-16 16:03:48 +1000

    feat: render reveal

--- UP2U-learn/frontend/src/pages/Reveal.tsx
+++ UP2U-learn/frontend/src/pages/Reveal.tsx
@@ -6,5 +6,32 @@ export default function Reveal() {
   const name = useLocation().state?.name;
   const reveal = useLocation().state?.reveal;
 
-  return <div>Reveal</div>;
+  return (
+    <div>
+      <h1>Reveal</h1>
+
+      <h2>{reveal.agreements}</h2>
+      <h2>{reveal.conflicts}</h2>
+      {Object.entries(reveal.personality_lines).map(([personName, line]) => (
+        <h2 key={[personName, line]}>
+          {personName}: {line}
+        </h2>
+      ))}
+
+      <h1>Decision:</h1>
+      <h2>
+        {reveal.primary.name}: {reveal.primary.reason}
+      </h2>
+      <a target="_blank" href={reveal.primary.maps_link}>
+        Open in Maps
+      </a>
+
+      <h1>Other Options:</h1>
+      {reveal.backups.map(({ name, reason }) => (
+        <h2 key={name}>
+          {name}: {reason}
+        </h2>
+      ))}
+    </div>
+  );
 }
