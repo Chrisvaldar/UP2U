ca24be100a02af4cc6fb2916fea803250dee50c2
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-19 21:23:20 +1000

    feat: disabled prop to button component

--- UP2U-learn/frontend/src/components/Button.tsx
+++ UP2U-learn/frontend/src/components/Button.tsx
@@ -1,13 +1,15 @@
 type ButtonProps = {
   label: string;
   onClick: () => void;
+  disabled?: boolean;
 };
 
-export default function Button({ label, onClick }: ButtonProps) {
+export default function Button({ label, onClick, disabled }: ButtonProps) {
   return (
     <button
       onClick={onClick}
       className="font-semibold bg-green-700 text-white px-6 py-3 rounded-full"
+      disabled={disabled}
     >
       {label}
     </button>
