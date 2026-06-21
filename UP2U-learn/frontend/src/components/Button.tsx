4e83074bd6b04ecb568ffbdebe9d3d116234a97d
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-21 20:23:08 +1000

    feat: outline button variant

--- UP2U-learn/frontend/src/components/Button.tsx
+++ UP2U-learn/frontend/src/components/Button.tsx
@@ -2,15 +2,21 @@ type ButtonProps = {
   label: string;
   onClick: () => void;
   disabled?: boolean;
+  variant?: string;
 };
 
-export default function Button({ label, onClick, disabled }: ButtonProps) {
+export default function Button({
+  label,
+  onClick,
+  disabled,
+  variant
+}: ButtonProps) {
+  const className =
+    variant === "outline"
+      ? "font-semibold border-2 border-green-700 text-green-700 px-6 py-3 rounded-full"
+      : "font-semibold bg-green-700 text-white px-6 py-3 rounded-full";
   return (
-    <button
-      onClick={onClick}
-      className="font-semibold bg-green-700 text-white px-6 py-3 rounded-full"
-      disabled={disabled}
-    >
+    <button onClick={onClick} className={className} disabled={disabled}>
       {label}
     </button>
   );
