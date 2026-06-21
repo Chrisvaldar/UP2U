a48f6aea80a62dd5b6dcf06049b23d1c1d2c688e
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-21 20:52:01 +1000

    feat: SortableItem component for cuisine survey

---
+++ UP2U-learn/frontend/src/components/SortableItem.tsx
@@ -0,0 +1,19 @@
+import { useSortable } from "@dnd-kit/react/sortable";
+
+type SortableItemProps = {
+  id: string;
+  index: number;
+};
+
+export default function Sortable({ id, index }: SortableItemProps) {
+  const { ref } = useSortable({ id, index });
+
+  return (
+    <div
+      ref={ref}
+      className="item bg-green-100 text-green-800 text-center px-4 py-2 mb-8 text-xl rounded-full"
+    >
+      {id}
+    </div>
+  );
+}
