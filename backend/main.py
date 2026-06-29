fef1fff13bf01f07509fd4f8c8adb65e37de8af8
Christopher Valensio Darsono <christophervalensio1@gmail.com>
2026-06-29 16:09:59 +1000

    chore: limit cors to deployed link and local

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 app = FastAPI(title="UP2U Learn")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://up2u-app.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
